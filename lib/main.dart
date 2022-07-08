43f8a25293f15c60df897a32aab4fe6f9b4677ea
Luxmi Gupta <73395071+luxmi1799@users.noreply.github.com>
2022-07-08 17:21:22 +0530

    comm

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:tutoro/home.dart';
 
 import 'login.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.amber,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.light,
-      darkTheme: ThemeData(
-          brightness: Brightness.dark
-      ),
-      // initialRoute: "/login",
-      // routes: {
-      // "/" :(context) => login(),
-      // }
-         home: SplashScreen(
-            seconds: 4,
-            navigateAfterSeconds: new login(),
-            title: new Text('',
-              style: new TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20.0
-              ),),
-            image: new Image.asset("assets/image/tutorologo.png"),
-            backgroundColor: Colors.white,
-            styleTextUnderTheLoader: new TextStyle(),
-            photoSize: 100.0,
-        )
+  State<StatefulWidget> createState() {
+     return _SplashScreenState();
+  }
+}
+
+class _SplashScreenState extends State<MyHomePage>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(seconds: (6)),
+      vsync: this,
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Lottie.asset(
+        'assets/image/lottie.json',
+        controller: _controller,
+        height: MediaQuery.of(context).size.height * 1,
+        animate: true,
+        onLoaded: (composition) {
+          _controller
+            ..duration = composition.duration
+            ..forward().whenComplete(() => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => login()),
+            ));
+        },
+      ),
+    );
+  }
 }
